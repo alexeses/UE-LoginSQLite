16f7a47973e7f0078b59f7e0bbfbdea77835d19c
Alex Gheorghe <alex@digitalsynware.com>
2022-05-06 23:47:32 +0200

    + add gui + add emojis

---
+++ src/main/java/gui/VLogin.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+public class VLogin {
+    private JLabel lblTitulo;
+    private JLabel lblUser;
+    private JPasswordField txtPass;
+    private JLabel lblPassword;
+    private JTextField txtUser;
+    private JButton btnAcceder;
+}
