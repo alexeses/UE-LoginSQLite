4aa837e3c17685f268085854c024b8a8c7c7edcd
Alex Gheorghe <alex@digitalsynware.com>
2022-05-07 13:42:56 +0200

    + build option java maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,14 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                VLogin vL = new VLogin();
+        EventQueue.invokeLater(() -> {
+            VLogin vL = new VLogin();
 
-                UsuarioPersistencia datos = new UsuarioPersistencia();
-                CLogin controlador = new CLogin(datos, vL);
-                vL.setControlador(controlador);
+            UsuarioPersistencia datos = new UsuarioPersistencia();
+            CLogin controlador = new CLogin(datos, vL);
+            vL.setControlador(controlador);
 
-                vL.hacerVisible();
-            }
+            vL.hacerVisible();
         });
     }
 }
